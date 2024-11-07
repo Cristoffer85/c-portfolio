3e57e8a58a2ab3373c934e7722a28b121ef0eba7
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-11-07 20:17:35 +0100

    Services component complete

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Backend development",
+        s_desc:"Build your next server running in the background with help of APIs and both relational and documentbased databases."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Frontend development",
+        s_desc:"Would you like a page similar to this one or another client? I can help with that."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Full stack development",
+        s_desc:"If you want to go full way with both a background server securely storing your data and frontend to display it I can help with that too."
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Graphics Design",
+        s_desc:"Believe it or not my first major orientation here in life starting at just 6 years old was actually aesthetical. So I´m actually not terrible at this as well."
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Game development",
+        s_desc:"What are you saying? Have you (almost) completed 2 games as well, one in Java and one in C++? Atleast i know the basics."
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Be a real good pal!",
+        s_desc:"Or if you just want a real good friend here you got me! Connect with me or hit me up on ByteBook! (Link in Footer) The only requirement is that you become my good pal too."
     },
 ]
  
