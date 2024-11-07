1573d8edcf5ac3eca4cad57b77533dc1e812fc1a
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-11-07 12:24:13 +0100

    Profile img fixed in GIMP and initial Hero started

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt="" />
+        <h1><span>I'm Cristoffer Östberg,</span> backend developer based in Sweden.</h1>
+        <p>I am a backend developer with experience in primarily Java, C#.NET and Python. And also alot of React (both .js and .ts) and HTML and CSS. I'm passionate about building scalable and efficient backend systems.</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect With Me</div>
+            <div className="hero-resume">My Resume</div>
+        </div>
+    </div>   
+  )
+}
+
+export default Hero
