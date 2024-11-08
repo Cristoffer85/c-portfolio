6dd3f9d4b5be2029c731131856bf1b64cb1d46e6
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-11-08 10:34:35 +0100

    Contact.css to 1:42:04 complete

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ import call_icon from '../../assets/call_icon.svg'
 
 const Contact = () => {
   return (
-    <div>
+        <div className="contact">
         <div className="contact-title">
             <h1>Get In Touch</h1>
             <img src={theme_pattern} alt=""/>
@@ -17,26 +17,28 @@ const Contact = () => {
                 <h1>Let's Talk</h1>
                 <p>Have one project or several in mind? I'd love to hear about it.</p>
                 <p>Drop me a message and I'll get back to you as soon as possible.</p>
-                <div className="contact-detail">
-                    <img src={mail_icon} alt="" /> <p>Cristoffer.Ostberg@gmail.com</p>
-                </div>
-                <div className="contact-detail">
-                    <img src={location_icon} alt="" /> <p>+46730453309</p>
-                </div>
-                <div className="contact-detail">
-                    <img src={call_icon} alt="" /> <p>Gothenburg, Sweden</p>
+                <div className="contact-details">
+                    <div className="contact-detail">
+                        <img src={mail_icon} alt="" /> <p>Cristoffer.Ostberg@gmail.com</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={call_icon} alt="" /> <p>+46730453309</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={location_icon} alt="" /> <p>Gothenburg, Sweden</p>
+                    </div>
                 </div>
             </div>
+            <form className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type="text" placeholder="Enter your name" name='name'/>
+                <label htmlFor="">Your Email</label>
+                <input type="email" placeholder="Enter your email" name='email'/>
+                <label htmlFor="">Write your message here!</label>
+                <textarea name="message" rows="8" placeholder="Enter your message"/>
+                <button type='submit' className="contact-submit">Submit now</button>
+            </form>
         </div>
-        <form className="contact-right">
-            <label htmlFor="">Your Name</label>
-            <input type="text" placeholder="Enter your name" name='name'/>
-            <label htmlFor="">Your Email</label>
-            <input type="email" placeholder="Enter your email" name='email'/>
-            <label htmlFor="">Write your message here!</label>
-            <textarea name="message" rows="8" placeholder="Enter your message"/>
-            <button type='submit' className="contact-submit">Submit now</button>
-        </form>
     </div>
   )
 }
