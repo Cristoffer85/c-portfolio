169e44a361f037d7ebe8e0f1b9a0cfbf9d5d6d79
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-11-09 14:43:35 +0100

    Anchorlink on other pages complete

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
         <h1><span>I'm Cristoffer Östberg,</span> backend developer based in Sweden.</h1>
         <p>I am a backend developer with experience in primarily Java, C#.NET and Python. In frontend React with both js and ts. I'm passionate about building visually appealing, scalable and efficient systems in every end.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect With Me</div>
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
         </div>
     </div>   
