61f2a11a7cff24cfda4df6061b39cabb0eaa46ec
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-11-09 14:32:42 +0100

    Navbar anchor linking done, styling left

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import profile_img from '../../assets/profile_img.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img} alt="" />
         <h1><span>I'm Cristoffer Östberg,</span> backend developer based in Sweden.</h1>
         <p>I am a backend developer with experience in primarily Java, C#.NET and Python. In frontend React with both js and ts. I'm passionate about building visually appealing, scalable and efficient systems in every end.</p>
