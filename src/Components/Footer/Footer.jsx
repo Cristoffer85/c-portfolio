9abe3f91bfa66fd0c9022e3b14389cea771a478d
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-11-09 14:01:15 +0100

    Footer styling complete

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="footer-top">
             <div className="footer-top-left">
                 <img src={footer_logo} alt="" />
-                <p>I am a backend developer from Sweden with experience in primarily Java, C#.NET and Python with React in frontend. Am passionate about building visually appealing scalable and efficient systems in every end.</p>
+                <p>I am a backend developer from Sweden with experience in primarily Java, C#.NET and React. Have various experience from both work and school.</p>
             </div>
             <div className="footer-top-right">
                 <div className="footer-email-input">
