fec9025e17887a0caed506e6f924465c1e808faa
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-12-10 12:41:25 +0100

    Portfolio top columns and links fixed (default screen size)

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import '../../Pages/Subpages.css';
+import '../Portfolio/Portfolio.css';
+
 import x from '../../assets/x.ico';
 import Githubrepomain from '../../assets/Githubrepomain.png';
-import { FaGithub } from 'react-icons/fa';
+
 import ByteBook from '../../assets/ByteBook.png'
 import Flakeville from '../../assets/Flakeville.png'
 import C from '../../assets/C.png'
@@ -11,6 +12,10 @@ import HoneyCombOdyssey from '../../assets/HoneyCombOdyssey.png'
 import Logodesign from '../../assets/Logodesign.png'
 import LateraRaceTracer from '../../assets/LateraRaceTracer.png'
 import SantaSelector from '../../assets/SantaSelector.png'
+
+import { FaGithub } from 'react-icons/fa';
+import { FiGithub } from "react-icons/fi";
+import { IoHomeOutline } from "react-icons/io5";
 
 const Portfolio = () => {
   const navigate = useNavigate();
@@ -37,103 +42,141 @@ const Portfolio = () => {
       <img src={x} alt="Back" className="back-icon" onClick={handleBackClick} />
       <div className="content-grid">
         <h1>Portfolio</h1>
-        <p>This is my portfolio page, where you above can view a small screenshot of the repository mainpage on Github and also a link to it.</p>
+        <p className="both-columns">My portfolio page - where you can view a screenshot from the repository mainpage on Github, and also a link to it.</p>
         <img src={Githubrepomain} alt="" />
 
-        <div className="contact-detail contact-detail-link">
-            <FaGithub /> <a href="https://github.com/Cristoffer85" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <div className="right-column link-container">
+            <FaGithub /> <a href="https://github.com/Cristoffer85" target="_blank" rel="noopener noreferrer">GITHUB MAIN</a>
         </div>
         
-        <h3>Listed below are different projects i have been working on for the last 2+ years:</h3>
-        <hr></hr>
+        <div className="blank-row both-columns"></div>
+        <h3 className="left-column">Projects listed:</h3>
+        <hr className="both-columns"></hr>
 
-        <h3 id="CO">CO</h3>
-        <p>This portfolio web application. Built completely as a pure frontend application.</p>
-        <img src={C} alt="" title="CO"/>
-        <h2 className="tech-knowledge">TECHNOLOGIES USED: React (Vite), JavaScript, Netlify</h2>
-        <hr></hr>
+          <h3 id="CO" className="left-column">CO</h3>
+          <p className="both-columns">This portfolio web application. Built completely as a pure frontend application.</p>
+          <img className="left-column" src={C} alt="" title="CO"/>
+          <div className="right-column link-container">
+            <h2>TECHNOLOGIES USED:</h2>
+            <p>React (Vite), JavaScript, Netlify</p>
+                <br /><br /><p>Github repo is private, but feel free to ask for access through the contact form.</p><br />
+                <IoHomeOutline /> <a href="https://co-po.netlify.app/" target="_blank" rel="noopener noreferrer">Co Portfolio</a>
+              </div>
+          <hr className="both-columns"></hr>
 
-        <h3 id="Santa-Selector">Santa Selector</h3>
-        <p>A festive in-between development of a duet mode Santa Selector app. A Simple and Tournament modestyle lets you decide who will at last be this Christmas Santa.</p>
-        <img src={SantaSelector} alt="" title="CO"/>
-        <h2 className="tech-knowledge">TECHNOLOGIES USED: React, TypeScript, Netlify</h2>
-        <hr></hr>
+          <h3 id="ByteBook" className="left-column">ByteBook</h3>
+          <p className="both-columns">A social media application for developers. Helping solve problems and just connect.</p>
+          <img className="left-column" src={ByteBook} alt="" title="ByteBook"/>
+            <div className="right-column link-container">
+            <h2>TECHNOLOGIES USED:</h2>
+              <p>C# .NET(8), React, TypeScript</p>
+              <br /><br /><p>Github repo is private, but feel free to ask for access through the contact form.</p><br />
+              <p>Undeployed - Currently in development</p>
+            </div>
+          <hr className="both-columns"></hr>
 
-        <h3 id="ByteBook">ByteBook</h3>
-        <p>A social media application for developers. Helping solve problems and just connect.</p>
-        <img src={ByteBook} alt="" title="ByteBook"/>
-        <h2 className="tech-knowledge">TECHNOLOGIES USED: C# .NET(8), React, TypeScript</h2>
-        <hr></hr>
+          <h3 id="Santa-Selector" className="left-column">Santa Selector</h3>
+          <p className="both-columns">A festive in-between development of a duet mode Santa Selector app. A Simple and Tournament modestyle lets you decide who will at last be this Christmas Santa.</p>
+          <img className="left-column" src={SantaSelector} alt="" title="Santa Selector"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>React, TypeScript, Netlify</p>
+              <br /><br /><p>Github repo is private, but feel free to ask for access through the contact form.</p><br />
+              <IoHomeOutline /> <a href="https://santaselector.netlify.app/" target="_blank" rel="noopener noreferrer">Santa Selector</a>
+            </div>
+          <hr className="both-columns"></hr>
 
-        <h3 id="FlakeVille">Flakeville</h3>
-        <p>A ski resort web application for both owners, employees and users. complete with IAM security and webshop.</p>
-        <img src={Flakeville} alt="" title="Flakeville"/>
-        <h2 className="tech-knowledge">TECHNOLOGIES USED: Java (Spring Boot), React, MongoDB, Azure</h2>
-        <hr></hr>
+          <h3 id="Logo-design-GIMP" className="left-column">FlakeVille</h3>
+          <p className="both-columns">A ski resort web application for both owners, employees and users. complete with IAM security and webshop.</p>
+          <img className="left-column" src={Flakeville} alt="" title="Flakeville"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java (Spring Boot), React, MongoDB, Azure</p>
+              <br /><br /><FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-server" target="_blank" rel="noopener noreferrer">Github link - Server</a><br />
+              <FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-client" target="_blank" rel="noopener noreferrer">Github link - Client</a><br /><br />
+              <IoHomeOutline /> <a href="https://flakeville.netlify.app/" target="_blank" rel="noopener noreferrer">FlakeVille</a>
+            </div>
+          <hr className="both-columns"></hr>
 
-        <h3 id="HoneyComb-Odyssey">HoneyComb Odyssey</h3>
-        <p>A 2D RPG game buit purely in Java Swing. The first complete game i finished during private time during the first year of education.</p>
-        <img src={HoneyCombOdyssey} alt="" title="HoneyComb Odyssey"/>
-        <h2 className="tech-knowledge">TECHNOLOGIES USED: Java (Swing)</h2>
-        <hr></hr>
+          <h3 id="HoneyComb-Odyssey" className="left-column">HoneyComb Odyssey</h3>
+          <p className="both-columns">A 2D RPG game built purely in Java Swing. The first complete game I finished during private time during the first year of education.</p>
+          <img className="left-column" src={HoneyCombOdyssey} alt="" title="HoneyComb Odyssey"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED: </h2>
+              <p>Java (Swing)</p>
+              <br /><br /><p>Github repo is private, but feel free to ask for access through the contact form.</p>
+            </div>
+          <hr className="both-columns"></hr>
 
-        <h3 id="Logo-design-GIMP">Logo Design</h3>
-        <p>Designing Various Logo:s and content in GIMP.</p>
-        <img src={Logodesign} alt="" title="HoneyComb Odyssey"/>
-        <h2 className="tech-knowledge">TECHNOLOGIES USED: GIMP</h2>
-        <hr></hr>
+          <h3 id="Logo-design-GIMP" className="left-column">Logo Design</h3>
+          <p className="both-columns">Designing Various Logos and content in GIMP.</p>
+          <img className="left-column" src={Logodesign} alt="" title="Logo Design"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>GIMP</p>
+            </div>
+          <hr className="both-columns"></hr>
 
-        <h3 id="Latera-Race-Tracer">Latera Race Tracer</h3>
-        <p>A desktop application for timing competitive RC-racing cars on a track with help of transponders and sensors. Built completely in Python and exciting project.</p>
-        <img src={LateraRaceTracer} alt="" title="Latera Race Tracer"/>
-        <h2 className="tech-knowledge">TECHNOLOGIES USED: Python (Kivy)</h2>
-        <hr></hr>
+          <h3 id="Latera-Race-Tracer" className="left-column">Latera Race Tracer</h3>
+          <p className="both-columns">A desktop application for timing competitive RC-racing cars on a track with help of transponders and sensors. Built completely in Python and exciting project.</p>
+          <img className="left-column" src={LateraRaceTracer} alt="" title="Latera Race Tracer"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+                <p>Python (Kivy)</p>
+                <br /><br /><FiGithub /> <a href="https://github.com/Cristoffer85/LRA" target="_blank" rel="noopener noreferrer">Github - Latera Race Tracer</a>
+              </div>
+          <hr className="both-columns"></hr>
 
-        <h4 className="full-span">Sample of Projects without a screenshot as of this moment, listed below:</h4>
-        <ul className="dotted-list">
-          <li>
-            <h3 id="Who slept the longest on christmas?">Who slept the longest on christmas?</h3>
-            <p>A terminal application with full IAM tracking a users sleep schedule last christmas. Editable by admin, of course.</p>
-            <h5>Main technologies: Java, Spring Boot, MongoDB</h5>
-          </li>
+        <h4 className="both-columns">Projects without a screenshot of this moment, listed:</h4>
+        
+          <h3 className="left-column">Who slept the longest on christmas?</h3>
+          <p className="left-column">A terminal application with full IAM tracking a users sleep schedule last christmas. Editable by admin, of course.</p>
+          <div className="right-column link-container">
+          <h2>TECHNOLOGIES USED:</h2>
+          <p>Java, Spring Boot, MongoDB</p>
+          </div>
 
-          <li>
-            <h3 id="Anectarchy">Anectarchy</h3>
-            <p>The second game currently in development.</p>
-            <h5>Main technologies: C++, SFML</h5>
-          </li>
+          <h3 className="left-column">Anectarchy</h3>
+          <p className="left-column">The second game currently in development.</p>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>C++, SFML</p>
+            </div>
 
-          <li>
-            <h3 id="ProblemSolvers E-store">ProblemSolvers E-store</h3>
-            <p>A group project in school creating our very first fullstack E-commerce application. Very dear project and group.</p>
-            <h5>Main technologies: Java, Spring Boot, Javascript and Typescript</h5>
-          </li>
+          <h3 className="left-column">ProblemSolvers E-store</h3>
+          <p className="left-column">A group project in school creating our very first fullstack E-commerce application. Very dear project and group.</p>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java, Spring Boot, Javascript and Typescript</p>
+            </div>
 
-          <li>
-            <h3 id="Socket Server-Client Play">Socket Server-Client Play</h3>
-            <p>Individual project creating socketbased communication between multiple clients (a chat) first one done with chats and very interesting. Start a chat-server in terminal and users can join.</p>
-            <h5>Main technologies: Java</h5>
-          </li>
+          <h3 className="left-column">Socket Server-Client Play</h3>
+          <p className="left-column">Individual project creating socketbased communication between multiple clients (a chat) first one done with chats and very interesting. Start a chat-server in terminal and users can join.</p>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java</p>
+            </div>
 
-          <li>
-            <h3 id="Apache Kafka BumbiBear App">Apache Kafka BumbiBear App</h3>
-            <p>Individual project creating a microservice with help of Apache Kafka. Save and view specific parameters of a BumbiBear through a Kafka Broker.</p>
-            <h5>Main technologies: Java, Apache Kafka, MySQL, MongoDB, local storage to .txt, JUnit, Mockito</h5>
-          </li>
+          <h3 className="left-column">Apache Kafka BumbiBear App</h3>
+          <p className="left-column">Individual project creating a microservice with help of Apache Kafka. Save and view specific parameters of a BumbiBear through a Kafka Broker.</p>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java, Apache Kafka, MySQL, MongoDB, local storage to .txt, JUnit, Mockito</p>
+            </div>
 
-          <li>
-            <h3 id="W40K CRUD">W40K CRUD</h3>
-            <p>My first project implementing CRUD. Since i in the background kind of like Warhammer as well i only found this fitting to make a starter project of.</p>
-            <h5>Main technologies: Java</h5>
-          </li>
+          <h3 className="left-column">W40K CRUD</h3>
+          <p className="left-column">My first project implementing CRUD. Since i in the background kind of like Warhammer as well i only found this fitting to make a starter project of.</p>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java</p>
+            </div>
 
-          <li>
-            <h3 id="Terminal Mayhem">Terminal Mayhem</h3>
-            <p>One of our first Group projects in school where we were supposed to create our own game. Built completely in terminal and features an RPG game following a character taking on monsters in an abnormally large airport terminal.</p>
-            <h5>Main technologies: Java</h5>
-          </li>
-        </ul>
-
+          <h3 className="left-column">Terminal Mayhem</h3>
+          <p className="left-column">One of our first Group projects in school where we were supposed to create our own game. Built completely in terminal and features an RPG game following a character taking on monsters in an abnormally large airport terminal.</p>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java</p>
+            </div>
       </div>
     </div>
   );
