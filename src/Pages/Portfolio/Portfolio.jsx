54d3de4888f2d7c91e74cd53b9a24ae87f60a214
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-12-10 12:56:20 +0100

    Lower columns fixed

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -125,58 +125,73 @@ const Portfolio = () => {
                 <p>Python (Kivy)</p>
                 <br /><br /><FiGithub /> <a href="https://github.com/Cristoffer85/LRA" target="_blank" rel="noopener noreferrer">Github - Latera Race Tracer</a>
               </div>
-          <hr className="both-columns"></hr>
+  
+          <div className="blank-row both-columns"></div>
 
-        <h4 className="both-columns">Projects without a screenshot of this moment, listed:</h4>
-        
+        <h4 className="both-columns">Projects without a screenshot (as of this moment) listed:</h4>
+          <hr className="both-columns"></hr>
           <h3 className="left-column">Who slept the longest on christmas?</h3>
           <p className="left-column">A terminal application with full IAM tracking a users sleep schedule last christmas. Editable by admin, of course.</p>
-          <div className="right-column link-container">
-          <h2>TECHNOLOGIES USED:</h2>
-          <p>Java, Spring Boot, MongoDB</p>
-          </div>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java, Spring Boot, MongoDB</p>
+              <br /><FiGithub /> <a href="https://github.com/Cristoffer85/WhoSleptTheLongestOnChristmas" target="_blank" rel="noopener noreferrer">Github - Who slept the longest on christmas?</a>
+            </div>
+          <hr className="both-columns"></hr>
 
           <h3 className="left-column">Anectarchy</h3>
           <p className="left-column">The second game currently in development.</p>
             <div className="right-column link-container">
               <h2>TECHNOLOGIES USED:</h2>
               <p>C++, SFML</p>
+              <br /><p>Github repo is private, but feel free to ask for access through the contact form.</p>
             </div>
+          <hr className="both-columns"></hr>
 
           <h3 className="left-column">ProblemSolvers E-store</h3>
           <p className="left-column">A group project in school creating our very first fullstack E-commerce application. Very dear project and group.</p>
             <div className="right-column link-container">
               <h2>TECHNOLOGIES USED:</h2>
               <p>Java, Spring Boot, Javascript and Typescript</p>
+              <br /><FiGithub /> <a href="https://github.com/Cristoffer85/Problemlosarna_E-Store" target="_blank" rel="noopener noreferrer">Github - ProblemSolvers E-store</a>
             </div>
+          <hr className="both-columns"></hr>
 
           <h3 className="left-column">Socket Server-Client Play</h3>
           <p className="left-column">Individual project creating socketbased communication between multiple clients (a chat) first one done with chats and very interesting. Start a chat-server in terminal and users can join.</p>
             <div className="right-column link-container">
               <h2>TECHNOLOGIES USED:</h2>
               <p>Java</p>
+              <br /><FiGithub /> <a href="https://github.com/Cristoffer85/SocketServerClientPlay" target="_blank" rel="noopener noreferrer">Github - Socket Server-Client Play</a>
             </div>
+          <hr className="both-columns"></hr>
 
           <h3 className="left-column">Apache Kafka BumbiBear App</h3>
           <p className="left-column">Individual project creating a microservice with help of Apache Kafka. Save and view specific parameters of a BumbiBear through a Kafka Broker.</p>
             <div className="right-column link-container">
               <h2>TECHNOLOGIES USED:</h2>
               <p>Java, Apache Kafka, MySQL, MongoDB, local storage to .txt, JUnit, Mockito</p>
+              <br /><FiGithub /> <a href="https://github.com/Cristoffer85/ApacheKafkaBumbiBearApp" target="_blank" rel="noopener noreferrer">Github - Apache Kafka BumbiBear App</a>
             </div>
+          <hr className="both-columns"></hr>
 
           <h3 className="left-column">W40K CRUD</h3>
           <p className="left-column">My first project implementing CRUD. Since i in the background kind of like Warhammer as well i only found this fitting to make a starter project of.</p>
             <div className="right-column link-container">
               <h2>TECHNOLOGIES USED:</h2>
               <p>Java</p>
+              <br /><FiGithub /> <a href="https://github.com/Cristoffer85/W40KCRUD-UTM3" target="_blank" rel="noopener noreferrer">Github - W40K CRUD</a>
             </div>
+          <hr className="both-columns"></hr>
 
           <h3 className="left-column">Terminal Mayhem</h3>
           <p className="left-column">One of our first Group projects in school where we were supposed to create our own game. Built completely in terminal and features an RPG game following a character taking on monsters in an abnormally large airport terminal.</p>
             <div className="right-column link-container">
               <h2>TECHNOLOGIES USED:</h2>
               <p>Java</p>
+              <br /><FiGithub /> <a href="https://github.com/Cristoffer85/Terminal_Mayhem" target="_blank" rel="noopener noreferrer">Github - Terminal Mayhem</a>
             </div>
+          <hr className="both-columns"></hr>
       </div>
     </div>
   );
