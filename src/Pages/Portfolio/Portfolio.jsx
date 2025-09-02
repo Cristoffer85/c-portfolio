88e1da7855dce417c0021622ede44066ceafb927
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-09-02 13:07:07 +0200

    Updated projects

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -8,12 +8,14 @@ import x from '../../assets/x.ico';
 import Githubrepomain from '../../assets/Githubrepomain.png';
 
 import ByteBook from '../../assets/ByteBook.png'
-import Flakeville from '../../assets/Flakeville.png'
+import Flakeville2 from '../../assets/Flakeville2.png'
 import C from '../../assets/C.png'
 import HoneyCombOdyssey from '../../assets/HoneyCombOdyssey.png'
 import Logodesign from '../../assets/Logodesign.png'
 import LateraRaceTracer from '../../assets/LateraRaceTracer.png'
 import SantaSelector from '../../assets/SantaSelector.png'
+import Projektatorn from '../../assets/Projektatorn.png'
+import Unnamed_game from '../../assets/Unnamed_game.png'
 
 import { FaGithub } from 'react-icons/fa';
 import { FiGithub } from "react-icons/fi";
@@ -54,19 +56,52 @@ const Portfolio = () => {
         
         <div className="blank-row both-columns"></div>
         <h3 className="left-column">Projects listed:</h3>
-        <hr className="both-columns"></hr>
+        <hr className="both-columns"></hr>    
+{/*Projektatorn -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
+          <h3 id="Projektatorn" className="left-column">Projektatorn</h3>
+          <p className="both-columns">Project generator, collaborative web application using GenAI. Built to promote programming collaboration and creation.</p>
+          <img className="left-column" src={Projektatorn} alt="" title="Projektatorn"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java, Angular, Google Gemini, RabbitMQ, MongoDB, Render.com, Netlify</p>
+              <br /><br />
+              
+              <FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-server" target="_blank" rel="noopener noreferrer">Github link - Server</a><br />
+              <FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-client" target="_blank" rel="noopener noreferrer">Github link - Client</a><br /><br />
 
+              <IoHomeOutline /> <a href="https://www.projektatorn.se/" target="_blank" rel="noopener noreferrer">Projektatorn</a>
+            </div>
+          <hr className="both-columns"></hr>
+{/*FlakeVille ------------------------------------------------------------------------------------------------------------------------------------------------------*/}
+          <h3 id="FlakeVille" className="left-column">FlakeVille</h3>
+          <p className="both-columns">A ski resort web application for both owners, employees and users. complete with IAM security and webshop.</p>
+          <img className="left-column" src={Flakeville2} alt="" title="Flakeville"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java (Spring Boot), React, RabbitMQ, MongoDB, Azure</p>
+              <br /><br />
+
+              <FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-server" target="_blank" rel="noopener noreferrer">Github link - Server</a><br />
+              <FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-client" target="_blank" rel="noopener noreferrer">Github link - Client</a><br /><br />
+
+              <IoHomeOutline /> <a href="https://flakeville.netlify.app/" target="_blank" rel="noopener noreferrer">FlakeVille</a>
+            </div>
+          <hr className="both-columns"></hr>
+{/*CO -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 id="CO" className="left-column">CO</h3>
           <p className="both-columns">This portfolio web application. Built completely as a pure frontend application.</p>
           <img className="left-column" src={C} alt="" title="CO"/>
             <div className="right-column link-container">
               <h2>TECHNOLOGIES USED:</h2>
               <p>React (Vite), JavaScript, Netlify</p>
-                  <br /><br /><p>* Github repo is private, but feel free to ask for access through the contact form.</p><br />
-                  <IoHomeOutline /> <a href="https://co-po.netlify.app/" target="_blank" rel="noopener noreferrer">Co Portfolio</a>
+              <br /><br />
+
+              <FiGithub /> <a href="https://github.com/Cristoffer85/c-portfolio" target="_blank" rel="noopener noreferrer">Github link</a><br /><br />
+
+              <IoHomeOutline /> <a href="https://co-po.netlify.app/" target="_blank" rel="noopener noreferrer">Co Portfolio</a>
             </div>
           <hr className="both-columns"></hr>
-
+{/*ByteBook -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 id="ByteBook" className="left-column">ByteBook</h3>
           <p className="both-columns">A social media application for developers. Helping solve problems and just connect.</p>
           <img className="left-column" src={ByteBook} alt="" title="ByteBook"/>
@@ -77,7 +112,20 @@ const Portfolio = () => {
               <p>Undeployed - Currently in development</p>
             </div>
           <hr className="both-columns"></hr>
+{/*Unnamed Game -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
+          <h3 id="Unnamed-Game" className="left-column">Unnamed Game</h3>
+          <p className="both-columns">Unnamed game with better gamephysics and easier scalability. Also built in swing but conversion to web/typescript is planned.</p>
+          <img className="left-column" src={Unnamed_game} alt="" title="Unnamed Game"/>
+            <div className="right-column link-container">
+              <h2>TECHNOLOGIES USED:</h2>
+              <p>Java (Swing)</p>
+              <br /><br />
+              
+              <p>* Github repo is private, but feel free to ask for access through the contact form.</p><br />
 
+            </div>
+          <hr className="both-columns"></hr>
+{/*Santa Selector -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 id="Santa-Selector" className="left-column">Santa Selector</h3>
           <p className="both-columns">A festive in-between development of a duet mode Santa Selector app. A Simple and Tournament modestyle lets you decide who will at last be this Christmas Santa.</p>
           <img className="left-column" src={SantaSelector} alt="" title="Santa Selector"/>
@@ -88,19 +136,7 @@ const Portfolio = () => {
               <IoHomeOutline /> <a href="https://santaselector.netlify.app/" target="_blank" rel="noopener noreferrer">Santa Selector</a>
             </div>
           <hr className="both-columns"></hr>
-
-          <h3 id="Logo-design-GIMP" className="left-column">FlakeVille</h3>
-          <p className="both-columns">A ski resort web application for both owners, employees and users. complete with IAM security and webshop.</p>
-          <img className="left-column" src={Flakeville} alt="" title="Flakeville"/>
-            <div className="right-column link-container">
-              <h2>TECHNOLOGIES USED:</h2>
-              <p>Java (Spring Boot), React, MongoDB, Azure</p>
-              <br /><br /><FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-server" target="_blank" rel="noopener noreferrer">Github link - Server</a><br />
-              <FiGithub /> <a href="https://github.com/Cristoffer85/Flakeville-client" target="_blank" rel="noopener noreferrer">Github link - Client</a><br /><br />
-              <IoHomeOutline /> <a href="https://flakeville.netlify.app/" target="_blank" rel="noopener noreferrer">FlakeVille</a>
-            </div>
-          <hr className="both-columns"></hr>
-
+{/*HoneyComb Odyssey -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 id="HoneyComb-Odyssey" className="left-column">HoneyComb Odyssey</h3>
           <p className="both-columns">A 2D RPG game built purely in Java Swing. The first complete game I finished during private time during the first year of education.</p>
           <img className="left-column" src={HoneyCombOdyssey} alt="" title="HoneyComb Odyssey"/>
@@ -110,7 +146,7 @@ const Portfolio = () => {
               <br /><br /><p>* Github repo is private, but feel free to ask for access through the contact form.</p>
             </div>
           <hr className="both-columns"></hr>
-
+{/*Logo Design GIMP -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 id="Logo-design-GIMP" className="left-column">Logo Design</h3>
           <p className="both-columns">Designing Various Logos and content in GIMP.</p>
           <img className="left-column" src={Logodesign} alt="" title="Logo Design"/>
@@ -119,7 +155,7 @@ const Portfolio = () => {
               <p>GIMP</p>
             </div>
           <hr className="both-columns"></hr>
-
+{/*Latera Race Tracer -----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 id="Latera-Race-Tracer" className="left-column">Latera Race Tracer</h3>
           <p className="both-columns">A desktop application for timing competitive RC-racing cars on a track with help of transponders and sensors. Built completely in Python and exciting project.</p>
           <img className="left-column" src={LateraRaceTracer} alt="" title="Latera Race Tracer"/>
@@ -130,7 +166,9 @@ const Portfolio = () => {
               </div>
   
           <div className="blank-row both-columns"></div>
-
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
+{/*###########################################################################################################################################################*/}
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
         <h4 className="both-columns">Projects without a screenshot (as of this moment) listed:</h4>
           <hr className="both-columns"></hr>
           <h3 className="left-column">Who slept the longest on christmas?</h3>
@@ -141,7 +179,7 @@ const Portfolio = () => {
               <br /><FiGithub /> <a href="https://github.com/Cristoffer85/WhoSleptTheLongestOnChristmas" target="_blank" rel="noopener noreferrer">Github - Who slept the longest on christmas?</a>
             </div>
           <hr className="both-columns"></hr>
-
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 className="left-column">Anectarchy</h3>
           <p className="left-column">The second game currently in development.</p>
             <div className="right-column link-container">
@@ -150,7 +188,7 @@ const Portfolio = () => {
               <br /><p>* Github repo is private, but feel free to ask for access through the contact form.</p>
             </div>
           <hr className="both-columns"></hr>
-
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 className="left-column">ProblemSolvers E-store</h3>
           <p className="left-column">A group project in school creating our very first fullstack E-commerce application. Very dear project and group.</p>
             <div className="right-column link-container">
@@ -159,7 +197,7 @@ const Portfolio = () => {
               <br /><FiGithub /> <a href="https://github.com/Cristoffer85/Problemlosarna_E-Store" target="_blank" rel="noopener noreferrer">Github - ProblemSolvers E-store</a>
             </div>
           <hr className="both-columns"></hr>
-
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 className="left-column">Socket Server-Client Play</h3>
           <p className="left-column">Individual project creating socketbased communication between multiple clients (a chat) first one done with chats and very interesting. Start a chat-server in terminal and users can join.</p>
             <div className="right-column link-container">
@@ -168,7 +206,7 @@ const Portfolio = () => {
               <br /><FiGithub /> <a href="https://github.com/Cristoffer85/SocketServerClientPlay" target="_blank" rel="noopener noreferrer">Github - Socket Server-Client Play</a>
             </div>
           <hr className="both-columns"></hr>
-
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 className="left-column">Apache Kafka BumbiBear App</h3>
           <p className="left-column">Individual project creating a microservice with help of Apache Kafka. Save and view specific parameters of a BumbiBear through a Kafka Broker.</p>
             <div className="right-column link-container">
@@ -177,7 +215,7 @@ const Portfolio = () => {
               <br /><FiGithub /> <a href="https://github.com/Cristoffer85/ApacheKafkaBumbiBearApp" target="_blank" rel="noopener noreferrer">Github - Apache Kafka BumbiBear App</a>
             </div>
           <hr className="both-columns"></hr>
-
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 className="left-column">W40K CRUD</h3>
           <p className="left-column">My first project implementing CRUD. Since i in the background kind of like Warhammer as well i only found this fitting to make a starter project of.</p>
             <div className="right-column link-container">
@@ -186,7 +224,7 @@ const Portfolio = () => {
               <br /><FiGithub /> <a href="https://github.com/Cristoffer85/W40KCRUD-UTM3" target="_blank" rel="noopener noreferrer">Github - W40K CRUD</a>
             </div>
           <hr className="both-columns"></hr>
-
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
           <h3 className="left-column">Terminal Mayhem</h3>
           <p className="left-column">One of our first Group projects in school where we were supposed to create our own game. Built completely in terminal and features an RPG game following a character taking on monsters in an abnormally large airport terminal.</p>
             <div className="right-column link-container">
@@ -195,6 +233,7 @@ const Portfolio = () => {
               <br /><FiGithub /> <a href="https://github.com/Cristoffer85/Terminal_Mayhem" target="_blank" rel="noopener noreferrer">Github - Terminal Mayhem</a>
             </div>
           <hr className="both-columns"></hr>
+{/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/}
       </div>
     </div>
   );
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,47 +1,49 @@
 import project1_img from '../assets/ByteBook.png'
-import project2_img from '../assets/Flakeville.png'
-import project3_img from '../assets/C.png'
+import project2_img from '../assets/Flakeville2.png'
 import project4_img from '../assets/HoneyCombOdyssey.png'
-import project5_img from '../assets/Logodesign.png'
-import project6_img from '../assets/LateraRaceTracer.png'
-import project7_img from '../assets/SantaSelector.png'
+import project5_img from '../assets/SantaSelector.png'
+import project7_img from '../assets/Unnamed_game.png'
+import project8_img from '../assets/Projektatorn.png'
+
+// import project3_img from '../assets/C.png'
+// import project6_img from '../assets/LateraRaceTracer.png'
 
 const mywork_data = [
     {
+        w_no:1,
+        w_name:"Projektatorn (Java, Angular, MongoDB, Render.com)",
+        w_img:project8_img,
+        id: "Projektatorn"
+    },
+    {
+        w_no:2,
+        w_name: "FlakeVille (Java, React, MongoDB)",
+        w_img: project2_img,
+        id: "FlakeVille"
+    },
+    {
         w_no:3,
-        w_name:"Portfolio (React -Vite, Netlify)",
-        w_img:project3_img,
-        id: "CO"
-    },
-    {
-        w_no: 1,
-        w_name: "ByteBook (C# .NET, React)",
-        w_img: project1_img,
-        id: "ByteBook"
-    },
-    {
-        w_no: 2,
-        w_name: "Santa Selector (React, TypeScript)",
-        w_img: project7_img,
-        id: "Santa-Selector"
-    },
-    {
-        w_no:4,
-        w_name:"HoneyComb Odyssey (Java -Swing",
+        w_name:"HoneyComb Odyssey (Java -Swing)",
         w_img:project4_img,
         id: "HoneyComb-Odyssey"
     },
     {
-        w_no: 5,
-        w_name: "Logo design (GIMP)",
-        w_img: project5_img,
-        id: "Logo-design-GIMP"
+        w_no:4,
+        w_name: "ByteBook (.NET, React, MSSQL)",
+        w_img: project1_img,
+        id: "ByteBook"
     },
     {
-        w_no: 6,
-        w_name: "Latera Race Tracer App (Python)",
-        w_img: project6_img,
-        id: "Latera-Race-Tracer"
+        w_no:5,
+        w_name: "Unnamed Game (Java -Swing)",
+        w_img: project7_img,
+        id: "Unnamed-Game"
+    },
+    {
+        w_no:6,
+        w_name: "Santa Selector (React)",
+        w_img: project5_img,
+        id: "Santa-Selector"
     },
 ]
  
