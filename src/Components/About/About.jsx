c04e35ca7101493e232f800f536adcb0f38cd320
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-09-02 11:15:22 +0200

    About updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -16,17 +16,18 @@ const About = () => {
         </div>
           <div className="about-right">
             <div className="about-para">
-                <p>I am a developer with up to a year work experience where mostly Java, C# and Python have been used.</p>
-                <p>My degree certificate is in Java where i was a very dedicated student and received among the highest degrees in class. I´ve created several projects both for school and work but also private where languages include Java, C#(.NET), Python, C++ and React (Javascript + Typescript)</p>
-                <p>The education programmes major was integration, where i picked up and learned alot about API:s, Databases, Microservices, Agile teamstructure and logic in general. I love to bring scalability and efficiency (and design with just a little UX as well) to applications and bring my best to every project.</p>
-                <p>Have 7+ years of consultancy experience, and come from a background regarding Pedagogy, Logistics and Security.</p>
+                <p>I am a developer with up to a year work experience where mostly Java, C# and AWS have been used.</p>
+                <p>My degree certificate is in Java where as a class representative was a dedicated student and received among highest degrees in class. Created several projects for both school, work and private, where languages include Java, C#(.NET), Python, C++ and React (Javascript + Typescript). Cloud technologies are mainly AWS and third-party like Render.com and Netlify.</p>
+                <p>Education programmes major was integration, where i picked up and learned alot about API:s, Databases, Microservices, Agile teamstructure and logic in general. I like to bring scalability and efficiency (and design with just a little UX as well) to applications and bring my best to every project.</p>
+                <p>Have 7+ years of consultancy experience, and come from a background regarding Security (degree from Swedish ministry of Civil Defense - MSB), Pedagogy and Logistics. Like clean code and collaboration - since i always want to avoid being the single-point of succes or failure.</p>
             </div>
             <div className="about-skills">
                 <div className="about-skill"><p>Java</p><hr className="hr-java" /></div>
+                <div className="about-skill"><p>AWS</p><hr className="hr-aws" /></div>
                 <div className="about-skill"><p>C# & .NET</p><hr className="hr-csharp" /></div>
                 <div className="about-skill"><p>Python</p><hr className="hr-python" /></div>
                 <div className="about-skill"><p>C++</p><hr className="hr-cpp" /></div>
-                <div className="about-skill"><p>React Js + Ts</p><hr className="hr-react" /></div>
+                <div className="about-skill"><p>Angular + React</p><hr className="hr-angular-react" /></div>
                 <div className="about-skill"><p>HTML & CSS</p><hr className="hr-htmlcss" /></div>
             </div>
           </div>
